751f52f4294e908b558134703f9bde2d2fc5da6b
Techoff09 <s.jarrouge@gmail.com>
2020-10-31 00:10:26 +0100

    frontend-tests>Integration:new folder pages: files indexPage.js & dashboard.js

---
+++ frontend-tests/cypress/pages/indexPage.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+// Elements
+const tiltleIndexPage = ''
+const usernameTextfield = ':nth-child(1) > input'
+const passwordTextFiels = ''
+const loginButton = ''
+
+
+
+// Actions / Functions
+
+// Exports
